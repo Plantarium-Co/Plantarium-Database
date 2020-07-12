c6f676b9391c12b01d42b58bbb83505a930ef465
Gabriel Ian Escober Abel <gabrielabel_69@yahoo.com>
2020-07-12 22:43:03 +0800

    Added create user stored procedure

---
+++ Plantarium.Database/Schemas/dbo/Programmability/Stored Procedures/sp_CreateUser.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[sp_CreateUser]
+	@identityId UNIQUEIDENTIFIER,
+	@givenName NVARCHAR(50),
+	@lastName NVARCHAR(50)
+AS
+	SET NOCOUNT ON
+	INSERT INTO [dbo].[Users] (IdentityId, GivenName, LastName)
+	VALUES (@identityId, @givenName, @lastName)
+RETURN 0
